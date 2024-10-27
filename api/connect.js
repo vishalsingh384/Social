2863ffb7c7dc54dddf9ccd09c73ab3363b4b015c
Singh <vishalsing384@gmail.com>
2024-10-27 16:22:02 +0530

    :bug: deployment jagran

--- api/connect.js
+++ api/connect.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const db=mysql.createConnection({
-    host:"localhost",
+    host:"127.0.0.1",
     user:"root",
     password:process.env.DB_PASS,
     database:"social"
